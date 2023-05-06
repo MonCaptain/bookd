573e58f1932529a15f068a5c902339f7c20b1e75
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-05-06 12:39:54 -0400

    feat: Created user model

--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    """
+    Account Manager to create differrent users
+    """
+
+    def create_user(self, username, first_name, last_name, password=None):
+        """
+        Creates a user account
+        """
+        if not username:
+            raise ValueError('Users must provide a username')
+
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.is_active = True
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, first_name, last_name, password=None):
+        """
+        Creates a superuser account
+        """
+        user = self.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password
+        )
+
+        user.is_staff = True
+        user.is_superuser = True
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    """
+    User Model
+    """
+
+    # User Detail Fields
+    username = models.CharField(max_length=20, unique=True, null=False)
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # User Type Fields
+    is_staff = models.BooleanField(default=False)
+
+    # Privacy Settings
+    visible_profile = models.BooleanField(default=True)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'username'
+
+    REQUIRED_FIELDS = [
+        'email',
+        'first_name',
+        'last_name',
+    ]
+
+    @property
+    def full_name(self) -> str:
+        """
+        Returns the user's full name
+        """
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return str(self.username)
