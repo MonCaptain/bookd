b3bbd6786f486686338e3c923b8cbb5f618748cf
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-05-08 21:44:54 -0400

    feat: Registered models in admin

--- server/books/admin.py
+++ server/books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+item_models = apps.get_app_config('books').get_models()
+
+for model in item_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
