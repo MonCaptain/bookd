d26508e3d0aa339195d617492d45a419521c7262
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-05-08 20:06:53 -0400

    feat: Created Book model

--- server/books/models.py
+++ server/books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    """
+    Book Model
+
+    Parameters
+    ----------
+    models : _type_
+        _description_
+    """
+    title = models.CharField(max_length=200, null=False)
+    page_count = models.PositiveSmallIntegerField()
+    publication_date = models.DateField()
+    cover_image = models.URLField()
