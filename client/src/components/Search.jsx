23d8a2545b72778ea304cdbd5e8c3313285e1a44
raynelfss <raynelfss@hotmail.com>
2023-05-10 00:29:38 -0400

    feat: rolling placeholder in searchBar. - Placeholder will switch every 5 seconds of inactivity

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,27 +1,43 @@
-import { Input, InputGroup, InputLeftElement, InputRightElement, Stack } from "@chakra-ui/react";
-import { SearchIcon,PhoneIcon } from "@chakra-ui/icons";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { useEffect, useState } from "react";
 
-export default function SearchBar({handleChange}) {
-    let possibleBookPrompts = [
-        'Harry Potter And The Philosopher\'s Stone',
-        'The Hunger Games',
-        'Don Quixote De La Mancha',
-        'Cronica De Una Muerte Anunciada',
-        'Divergent',
-        'Frankenstein',
-        'To Kill a Mockingbird',
-        'The Green Mile',
-        'At The Mountains Of Madness'
-    ]
-    return (
-        <InputGroup mx={"9"}>
-            <InputLeftElement
-                pointerEvents='none'
-                children={<SearchIcon color='gray.300' />}
-            />
-            <Input type='text' 
-            placeholder={possibleBookPrompts[Math.floor(Math.random()*possibleBookPrompts.length)]}
-            onInput={handleChange}/>
-        </InputGroup>
-    )
+export default function SearchBar({ handleChange }) {
+  let possibleBookPrompts = [
+    "Harry Potter And The Philosopher's Stone",
+    "The Hunger Games",
+    "Don Quixote De La Mancha",
+    "Cronica De Una Muerte Anunciada",
+    "Divergent",
+    "Frankenstein",
+    "To Kill a Mockingbird",
+    "The Green Mile",
+    "At The Mountains Of Madness",
+  ];
+  let [suggestion, setSuggestion] = useState(
+    possibleBookPrompts[Math.floor(Math.random() * possibleBookPrompts.length)]
+  );
+
+  useEffect(() => {
+    let suggestTimeout = setTimeout(
+      () =>
+        setSuggestion(
+          possibleBookPrompts[
+            Math.floor(Math.random() * possibleBookPrompts.length)
+          ]
+        ),
+      5000
+    );
+    return () => clearTimeout(suggestTimeout);
+  });
+
+  return (
+    <InputGroup mx={"9"}>
+      <InputLeftElement
+        pointerEvents="none"
+        children={<SearchIcon color="gray.300" />}
+      />
+      <Input type="text" placeholder={suggestion} onInput={handleChange} />
+    </InputGroup>
+  );
 }
