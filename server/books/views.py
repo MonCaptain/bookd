94edb22de41670da93c376471693c54aadf06774
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-05-10 21:52:10 -0400

    feat: Added all endpoints

--- server/books/views.py
+++ server/books/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.apps import apps
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status
@@ -15,17 +14,18 @@ from .serializers import PublicProfileSerializer, PrivateProfileSerializer
 
 
 User = get_user_model()
-Profile = apps.get_model('books', 'Profile')
 
 
 class ManageUserProfiles(APIView):
     """
-    An API View class for retrieving user profile
+    An API View class for managing user profiles
 
     Methods
     -------
-    get(request, username)
-        Receives a GET request to retrieve a user profile
+    get
+        Retrieve a user profile
+    patch
+        Edit a user profile
     """
 
     def get(self, request, username):
@@ -85,11 +85,364 @@ class ManageUserProfiles(APIView):
         return Response(status=status.HTTP_200_OK)
 
 
+class ManageUserBookEntries(APIView):
+    """
+    An API View class for managing book entries
+
+    Methods
+    -------
+    get
+        Retrieve book entries from a user
+    post
+        Create a new book entry
+    """
+
+    def get(self, request, username):
+        """
+        Handles a GET request to retrieve a book entry
+        """
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+
+        book_list = user_profile.book_list.all()
+        serializer = BookEntrySerializer(book_list, many=True)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def post(self, request, username):
+        """
+        Handle a POST request to create a new book entry
+        """
+        requester = request.user
+        if requester.username != username:
+            return Response(
+                {'error': 'Only users who owns this profile can edit it'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+
+        data = request.data
+
+        book_id = data.get('book_id')
+
+        if not book_id:
+            return Response(
+                {'error': 'Please enter the book_id'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        book = get_object_or_404(Book, id=book_id)
+        book_entry = BookEntry.objects.create(
+            book=book,
+            profile=user_profile
+        )
+
+        serializer = BookEntrySerializer(book_entry)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+
 class ManageUserCollections(APIView):
+    """
+    An API View class for managing a user collection
 
-    def get(self, request, username, **kwargs):
-        pass
+    Methods
+    -------
+    get
+        Retrieve a list of collections from someone's profile
+    post
+        Create a new collection
+    """
+
+    def get(self, request, username):
+        """
+        Handles a GET request for getting all user collections
+        """
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+
+        collections = user_profile.collections.all()
+        serializer = CollectionSerializer(collections, many=True)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def post(self, request, username):
+        """
+        Handles a POST request to create a user collection
+        """
+        requester = request.user
+        if requester.username != username:
+            return Response(
+                {'error': 'Only users who owns this profile can edit it'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+
+        data = request.data
+
+        title = data.get('title')
+        description = data.get('description')
+
+        if not (title and description):
+            return Response(
+                {'error': 'At least one of the fields is missing'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        duplicate = Collection.objects.filter(title=title)
+        if duplicate.exists():
+            title = title + f"({duplicate.count() + 1})"
+
+        collection = Collection.objects.create(
+            profile=user_profile,
+            title=title,
+            description=description
+        )
+
+        user_profile.collections.add(collection)
+        serializer = CollectionSerializer(collection)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
 
-class ManageBooks(APIView):
-    pass
+class ManageBookEntryDetail(APIView):
+    """
+    An API View class for managing book entry details
+
+    Methods
+    -------
+    get
+        Retrieve a user entry detail
+    patch
+        Edit a user's entry detail
+    delete
+        Deletes a user entry detail
+    """
+
+    def get(self, request, username, entry_id):
+        """
+        Handles a GET request to retrieve a user entry detail
+        """
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+        book_entry = get_object_or_404(
+            BookEntry, id=entry_id, profile=user_profile)
+
+        serializer = BookEntrySerializer(book_entry)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def patch(self, request, username, entry_id):
+        """
+        Handles a PATCH request for editing a user entry detail
+        """
+        requester = request.user
+        if requester.username != username:
+            return Response(
+                {'error': 'Only users who owns this profile can edit it'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+
+        data = request.data
+
+        current_page = data.get('current_page')
+        book_status = data.get('status')
+
+        if current_page:
+            BookEntry.objects.filter(id=entry_id, profile=user_profile).update(
+                current_page=current_page)
+        if book_status:
+            BookEntry.objects.filter(
+                id=entry_id, profile=user_profile).update(status=book_status)
+
+        book_entry = BookEntry.objects.get(id=entry_id, profile=user_profile)
+        serializer = BookEntrySerializer(book_entry)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def delete(self, request, username, entry_id):
+        """
+        Handles a DELETE request for deleting a user entry detail
+        """
+        requester = request.user
+        if requester.username != username:
+            return Response(
+                {'error': 'Only users who owns this profile can edit it'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+        book_entry = get_object_or_404(
+            BookEntry, id=entry_id, profile=user_profile)
+
+        book_entry.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ManageUserCollectionDetail(APIView):
+    """
+    An API View class for managing user collection details
+
+    Methods
+    -------
+    get
+        Retrieve a user collection
+    patch
+        Edit a user collection
+    delete
+        Delete a user collection
+    """
+
+    def get(self, request, username, collection_id):
+        """
+        Handles a GET request for retrieving a user collection
+        """
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+        collection = get_object_or_404(
+            Collection, id=collection_id, profile=user_profile)
+
+        serializer = CollectionSerializer(collection)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def patch(self, request, username, collection_id):
+        """
+        Handles a PATCH request for editing a user collection
+        """
+        requester = request.user
+        if requester.username != username:
+            return Response(
+                {'error': 'Only users who owns this profile can edit it'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+
+        data = request.data
+
+        title = data.get('title')
+        description = data.get('description')
+
+        if title:
+            Collection.objects.filter(
+                id=collection_id, profile=user_profile).update(title=title)
+        if description:
+            Collection.objects.filter(id=collection_id, profile=user_profile).update(
+                description=description)
+
+        collection = Collection.objects.get(
+            id=collection_id, profile=user_profile)
+        serializer = BookEntrySerializer(collection)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def delete(self, request, username, collection_id):
+        """
+        Handles a DELETE request for deleting a user collection
+        """
+        requester = request.user
+        if requester.username != username:
+            return Response(
+                {'error': 'Only users who owns this profile can edit it'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = get_object_or_404(User, username=username)
+        user_profile = get_object_or_404(Profile, user=user)
+        collection = get_object_or_404(
+            BookEntry, id=collection_id, profile=user_profile)
+
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CreateBook(APIView):
+    """
+    An API View class for creating a book
+
+    Methods
+    -------
+    post
+        Creates/get a book object
+    """
+
+    def post(self, request):
+        """
+        Handles a POST request for creating/getting a new book object
+        """
+        data = request.data
+
+        title = data.get('title')
+        author = data.get('author')
+        page_count = data.get('page_count')
+        publication_date = data.get('publication_date')
+        cover_image = data.get('cover_image')
+
+        if not (title and author and page_count and publication_date and cover_image):
+            return Response(
+                {'error': 'At least one of the fields is missing'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        book = Book.objects.get_or_create(
+            title=title,
+            author=author,
+            page_count=page_count,
+            publication_date=publication_date,
+            cover_image=cover_image
+        )
+
+        serializer = BookSerializer(book)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+
+class BookDetail(APIView):
+    """
+    An API View class for retrieving a book detail
+
+    Methods
+    -------
+    get
+        Retrieve a book detail
+    """
+
+    def get(self, request, book_id):
+        """
+        Handles a POST request for retrieving a book detail
+        """
+        book = get_object_or_404(Book, id=book_id)
+        serializer = BookSerializer(book)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
