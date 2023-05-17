909ac18e2e5f27265b84ed113e7c3e5435e0b77f
ShehabMohsen <aquashehab612@gmail.com>
2023-05-17 00:42:38 -0400

    feat: style explore user profiles card grid

--- client/src/components/UserProfileCard.jsx
+++ client/src/components/UserProfileCard.jsx
@@ -22,15 +22,7 @@ export default function UserProfileCard() {
         rounded={"md"}
         overflow={"hidden"}
       >
-        <Image
-          h={"120px"}
-          w={"full"}
-          src={
-            "https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-          }
-          objectFit={"cover"}
-        />
-        <Flex justify={"center"} mt={-12}>
+        <Flex justify={"center"} mt={5}>
           <Avatar
             size={"xl"}
             src={
@@ -44,40 +36,24 @@ export default function UserProfileCard() {
         </Flex>
 
         <Box p={6}>
-          <Stack spacing={0} align={"center"} mb={5}>
+          <Stack spacing={0} align={"center"}>
             <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
-              John Doe
+              Username
             </Heading>
-            <Text color={"gray.500"}>Frontend Developer</Text>
-          </Stack>
-
-          <Stack direction={"row"} justify={"center"} spacing={6}>
-            <Stack spacing={0} align={"center"}>
-              <Text fontWeight={600}>23k</Text>
-              <Text fontSize={"sm"} color={"gray.500"}>
-                Followers
-              </Text>
-            </Stack>
-            <Stack spacing={0} align={"center"}>
-              <Text fontWeight={600}>23k</Text>
-              <Text fontSize={"sm"} color={"gray.500"}>
-                Followers
-              </Text>
-            </Stack>
+            <Text color={"gray.500"}>37 book entries</Text>
           </Stack>
 
           <Button
             w={"full"}
-            mt={8}
-            bg={useColorModeValue("#151f21", "gray.900")}
-            color={"white"}
+            mt={6}
+            colorScheme="orange"
             rounded={"md"}
             _hover={{
               transform: "translateY(-2px)",
               boxShadow: "lg",
             }}
           >
-            Follow
+            View Profile
           </Button>
         </Box>
       </Box>
