f2914ca4b94bf66c95cbbf9d7b54a1fdee47559b
ShehabMohsen <aquashehab612@gmail.com>
2023-05-16 23:54:03 -0400

    feat: set up explore user profiles page scaffolding

---
+++ client/src/pages/ExploreUsersPage.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import UserProfileCard from "../components/UserProfileCard";
+
+export default function ExploreUsersPage() {
+  return (
+    <Box>
+      <UserProfileCard />
+    </Box>
+  );
+}
