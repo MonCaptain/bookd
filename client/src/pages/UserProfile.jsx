bdbde1d7f6e9c061520289bf4fad114b4eeb2608
ShehabMohsen <aquashehab612@gmail.com>
2023-05-17 03:07:51 -0400

    feat: set up user profile page HTML structure

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -21,6 +21,9 @@ export default function UserProfile({ isOriginalUser = false }) {
   isOriginalUser = isOriginalUser || usernameParams === authedUser.username;
   const requestParams = isOriginalUser ? authedUser.username : usernameParams;
 
+  const [userProfile, setUserProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     async function fetchUserProfile() {
       const responseData = await apiClient.getUserProfile(requestParams);
@@ -32,6 +35,47 @@ export default function UserProfile({ isOriginalUser = false }) {
 
   return (
     <>
+      {isLoading ? (
+        ""
+      ) : (
+        <Box>
+          {/* Display user states and fun fact information */}
+          <Heading mb={"20px"}>User Profile</Heading>
+          <Flex
+            columnGap={"50px"}
+            height="full"
+            maxHeight={"30vh"}
+            padding={"20px"}
+            bg={useColorModeValue("whiteAlpha.900", "gray.800")}
+            borderRadius={"5px"}
+          >
+            <Image
+              boxSize="250px"
+              objectFit="cover"
+              src={`http://localhost:8000${userProfile.profile_picture}`}
+              alt="Dan Abramov"
+            />
+
+            <Box>
+              <p>{userProfile.user.username}</p>
+              <p>Favorite Book: {userProfile.favorite_book.title}</p>
+              <p>{userProfile.book_list.length} book entries</p>
+              <Stack direction="row" columnGap={10}>
+                <Text>Set Private Profile?</Text>
+                <Switch
+                  colorScheme="orange"
+                  size="lg"
+                  isChecked={userProfile.private}
+                />
+              </Stack>
+            </Box>
+          </Flex>
+          {/* Display user books list */}
+          <Box mt={"20px"}>
+            <Heading >Book List</Heading>
+          </Box>
+        </Box>
+      )}
     </>
   );
 }
