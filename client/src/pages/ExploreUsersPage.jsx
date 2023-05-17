b5c9893156ec5203b941c0185c4e205d2b935de4
ShehabMohsen <aquashehab612@gmail.com>
2023-05-17 00:42:38 -0400

    feat: style explore user profiles card grid

--- client/src/pages/ExploreUsersPage.jsx
+++ client/src/pages/ExploreUsersPage.jsx
@@ -1,10 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid, Center, Flex } from "@chakra-ui/react";
 import UserProfileCard from "../components/UserProfileCard";
 
 export default function ExploreUsersPage() {
   return (
-    <Box>
-      <UserProfileCard />
-    </Box>
+    <Flex justifyContent={"center"}>
+      <Box maxWidth={1200} width={"full"}>
+      <SimpleGrid  columns={[1, 2, 3]} spacingX={'40px'} spacingY={"10px"}>
+        <UserProfileCard />
+        <UserProfileCard />
+        <UserProfileCard />
+        <UserProfileCard />
+        <UserProfileCard />
+        <UserProfileCard />
+        <UserProfileCard />
+      </SimpleGrid>
+      </Box>
+    </Flex>
   );
 }
