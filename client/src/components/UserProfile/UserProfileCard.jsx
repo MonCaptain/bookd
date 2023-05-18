2d3fc14213778aa7d2b8f81851cddc1ef1efea11
ShehabMohsen <aquashehab612@gmail.com>
2023-05-18 17:06:51 -0400

    feat: implement explore users page logic

--- client/src/components/UserProfile/UserProfileCard.jsx
+++ client/src/components/UserProfile/UserProfileCard.jsx
@@ -10,8 +10,9 @@ import {
   Button,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
-export default function UserProfileCard() {
+export default function UserProfileCard({ username, bookEntryCount, avatar }) {
   return (
     <Center py={6}>
       <Box
@@ -25,9 +26,7 @@ export default function UserProfileCard() {
         <Flex justify={"center"} mt={5}>
           <Avatar
             size={"xl"}
-            src={
-              "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-            }
+            src={`http://localhost:8000${avatar}`}
             alt={"Author"}
             css={{
               border: "2px solid white",
@@ -38,23 +37,29 @@ export default function UserProfileCard() {
         <Box p={6}>
           <Stack spacing={0} align={"center"}>
             <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
-              Username
+              {username}
             </Heading>
-            <Text color={"gray.500"}>37 book entries</Text>
+            <Text color={"gray.500"}>
+              {" "}
+              {bookEntryCount > 0
+                ? `${bookEntryCount} entries`
+                : "no entries"}{" "}
+            </Text>
           </Stack>
-
-          <Button
-            w={"full"}
-            mt={6}
-            colorScheme="orange"
-            rounded={"md"}
-            _hover={{
-              transform: "translateY(-2px)",
-              boxShadow: "lg",
-            }}
-          >
-            View Profile
-          </Button>
+          <Link to={`${username}`}>
+            <Button
+              w={"full"}
+              mt={6}
+              colorScheme="orange"
+              rounded={"md"}
+              _hover={{
+                transform: "translateY(-2px)",
+                boxShadow: "lg",
+              }}
+            >
+              View Profile
+            </Button>
+          </Link>
         </Box>
       </Box>
     </Center>
