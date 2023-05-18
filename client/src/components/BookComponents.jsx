d796e782ecaf4dd40c2d4950563c98a7ea389746
raynelfss <raynelfss@hotmail.com>
2023-05-17 23:12:08 -0400

    feat: bookCard for entries

--- client/src/components/BookComponents.jsx
+++ client/src/components/BookComponents.jsx
@@ -1,49 +1,96 @@
 /* eslint-disable react/prop-types */
 import {
   Button,
+  ButtonGroup,
   Card,
   CardBody,
   CardFooter,
-  CardHeader,
+  Flex,
   Heading,
+  Icon,
   Image,
+  Link,
+  Progress,
+  SimpleGrid,
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
-export default function BookCard({ cover, author, pagecount, title }) {
+export default function BookCard({ cover, author, currentPage, pagecount, title, progress, dropped, rating }) {
+  let ratingStars = []
+  for (let index = 0; index < 5; index++) {
+    ratingStars.push(<Icon as={(index < rating) ? AiFillStar : AiOutlineStar} boxSize={8} key={index} color={'#faaf00'}/>)
+  }
   return (
-    <Card
-      minW={"500px"}
-      direction={{ base: "column", sm: "row" }}
-      overflow="hidden"
-      variant="outline"
-    >
-      <Image
-        objectFit="cover"
-        maxW={{ base: "100%", sm: "200px" }}
-        src={cover}
-        alt={title}
-      />
-      <Stack>
-        <CardHeader>
-          <Heading size={"lg"}>{title}</Heading>
-          <Heading size={"md"} py="2">
-            Written by {author}
-          </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>
-            {pagecount ? `${pagecount} pages` : "Page count unavailable."}
-          </Text>
-        </CardBody>
-
-        <CardFooter>
-          <Button variant="solid" colorScheme="blue">
-            Start
-          </Button>
-        </CardFooter>
-      </Stack>
-    </Card>
+    <>
+      <Card
+        direction={{ base: "row", md: "column" }}
+        rounded={"xl"}
+        minH={{ base: "230px", sm: "100%" }}
+        alignItems={"center"}
+        justifyContent={{ base: "center", md: "space-between" }}
+      >
+        <Flex
+          justifyContent={"center"}
+          h={{ base: "100%", md: "" }}
+          w={{ base: "200px", md: "100%" }}
+        >
+          <Image
+            src={cover}
+            alt={author}
+            roundedTopLeft={"xl"}
+            roundedBottomLeft={{ base: "xl", md: "0" }}
+            roundedTopRight={{ base: "0", md: "xl" }}
+            h={{ base: "100%", md: "350px" }}
+            objectFit={"cover"}
+            objectPosition={"center"}
+            filter={dropped ? "grayscale(100%)" : ""}
+            w={{ base: "200px", md: "100%" }}
+          />
+        </Flex>
+        <Flex direction={"column"} alignContent={"end"} w={"100%"}>
+          <CardBody>
+            <Stack>
+              <Heading size="md" m={0} overflowWrap={"normal"}>
+                {title.slice(0, 64)}
+              </Heading>
+              <Text m={"0!important"}>{author ? author : "N/A"}</Text>
+              <Text color={progress === 100 ? "green" : "orange"} fontSize="2xl">
+                {currentPage ? `${currentPage}/${pagecount} Pages` : ""}
+              </Text>
+              { 
+                progress ? 
+                ( progress == 100 ?
+                  <Progress value={progress} rounded={'full'} colorScheme="green"/>:
+                  <Progress value={progress} rounded={'full'} colorScheme="orange"/>) : 
+                <Progress colorScheme={'orange'} rounded={'full'} isIndeterminate/>
+              }
+              <Stack mt={"5!important"} mb={0} direction={{base: "row", md: "column"}}>
+                <Text fontSize={'lg'} fontWeight={"500"}>Rating:</Text>
+                <SimpleGrid columns={5} alignSelf={'center'} spacingX={3}>
+                  {ratingStars}
+                </SimpleGrid>
+              </Stack>
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button
+                variant="solid"
+                colorScheme="orange"
+              >
+                Track
+              </Button>
+              <Button variant="ghost" colorScheme="orange">
+                <Link isExternal>
+                  More Info
+                </Link>
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Flex>
+      </Card>
+    </>
   );
 }
--- client/src/pages/BookList.jsx
+++ client/src/pages/BookList.jsx
@@ -1,27 +1,55 @@
-import { Box, Heading, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { Box, Heading, SimpleGrid, SlideFade, useColorModeValue } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import apiClient from "../services/apiClient";
 import BookCard from "../components/BookComponents";
 
-export default function BookList({ pageTitle }) {
+export default function BookList({ pageTitle, category }) {
   // fetch user book list, and then filter them based on reading status
   // i.e. currently_reading, completed, dropped, all, etc...
-  useEffect(() => {}, []);
 
-  let bookEntries = []
-  Promise.resolve(apiClient.retrieveEntries()).then((results) => {
-    bookEntries = results.map((bookEntry, index) => {
-      let book = bookEntry.book;
-      return (<BookCard cover={book.cover_image} title={book.title} pagecount={book.page_count} key={index}></BookCard>)
-    })
-  })
+  let [bookEntries, setBookEntries] = useState([]);
+
+  const fetchEntries = () => 
+  {
+    Promise.resolve(apiClient.retrieveEntries()).then((results) => {
+      let fetchedEntries = results.map((bookEntry, index) => {
+        if (bookEntry.status === category) {
+        return <BookCard 
+          cover={bookEntry.book.cover_image} 
+          title={bookEntry.book.title} 
+          author={bookEntry.book.author}
+          currentPage={bookEntry.current_page}
+          pagecount={bookEntry.book.page_count}
+          progress={(bookEntry.current_page/bookEntry.book.page_count)*100}
+          rating={bookEntry.rating}
+          key={index}>
+        </BookCard>
+        }})
+        setBookEntries(fetchedEntries)
+      })
+  }
+
+  useEffect(() => {
+    setBookEntries([])
+    fetchEntries();
+  }, [category]);
 
   return (
     <Box minW={'100%'} minH={'85vh'} bg={useColorModeValue('white', 'gray.900')} p={5}>
       <Heading>{pageTitle}</Heading>
-      <SimpleGrid>
-        {bookEntries}
-      </SimpleGrid>
+      <SlideFade
+              in={bookEntries.length > 0}
+              out={bookEntries.length === 0}
+            >
+        <SimpleGrid spacing={10}
+                alignItems={"center"}
+                justifyContent={"center"}
+                columns={[1, null, 2, 3, 4, null]}
+                gridRow={'unset'}
+                p={5}>
+          {bookEntries}
+        </SimpleGrid>
+      </SlideFade>
     </Box>
   );
 }
