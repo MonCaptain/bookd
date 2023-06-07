245db2794a08f18f61a2921314635e9561ed8501
Shehab Mohsen <97417536+ShehabMohsen@users.noreply.github.com>
2023-06-06 20:27:57 -0400

    feat: implement footer component for unauthenticated users (#55)

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,80 @@
+import {
+  Box,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue,
+  Image,
+  HStack,
+  Spacer,
+} from "@chakra-ui/react";
+import React from "react";
+import OrangeGitHubLogo from "../assets/OrangeGitHubLogo.png";
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function Footer() {
+  return (
+    <Box
+      bg={useColorModeValue("gray.100", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+      mt={"auto"}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr", md: "2fr 2fr 2fr" }}
+          spacing={8}
+        >
+          {/* SECTION 1 */}
+          <Stack spacing={6} align="center">
+            <Box>
+              <HStack>
+                <Text
+                  textColor={useColorModeValue("orange.500", "orange.400")}
+                  fontWeight={"bold"}
+                >
+                  Get Book'd
+                </Text>
+              </HStack>
+            </Box>
+            <Text fontSize={"sm"}>© 2023 Get Book'd . All rights reserved</Text>
+          </Stack>
+          {/* Section 2 */}
+          <Stack align={"center"}>
+            <ListHeader>Our Developers</ListHeader>
+            <HStack>
+              <Link href={"https://github.com/ShehabMohsen"} isExternal>
+                Shehab Mohsen
+              </Link>
+              <Spacer />
+              <Link href={"https://github.com/raynelfss"} isExternal>
+                Raynel Sanchez
+              </Link>
+            </HStack>
+            <HStack>
+              <Link href={"https://github.com/jedrm"} isExternal>
+                Jed Rendo Magracia
+              </Link>
+            </HStack>
+          </Stack>
+
+          {/* SECTION 3 */}
+          <Stack align={"center"}>
+            <ListHeader>Check us out on GitHub!</ListHeader>
+            <Link href={"https://github.com/ShehabMohsen/get-bookd"} isExternal>
+              <Image src={OrangeGitHubLogo} width={10} />
+            </Link>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,12 +1,14 @@
+import Footer from "../components/Footer";
 import SidebarWithHeader from "../components/SidebarWithHeader";
 import { useAuthContext } from "../contexts/AuthContext";
 import { useLocation } from "react-router-dom";
+import { Flex, useColorModeValue } from "@chakra-ui/react";
 
 export default function RootLayout({ children }) {
   const authVariables = useAuthContext();
   const isUserAuthed = authVariables.isUserAuthed;
   // do not render SideBarWithHeader if the user attempts to navigate to a route that doesn't exist
-  const location = useLocation()
+  const location = useLocation();
   const isNotFound = !(
     location.pathname === "/" ||
     location.pathname === "/login" ||
@@ -24,6 +26,13 @@ export default function RootLayout({ children }) {
   return isUserAuthed && !isNotFound ? (
     <SidebarWithHeader>{children}</SidebarWithHeader>
   ) : (
-    <>{children}</>
+    <Flex
+      flexDirection={"column"}
+      minH={"100vh"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      {children}
+      <Footer />
+    </Flex>
   );
 }
