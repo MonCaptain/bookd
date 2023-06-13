1523c2006d21ff84168fb7a2d2e7ec248c5b64b3
ShehabMohsen <aquashehab612@gmail.com>
2023-06-13 03:45:33 -0400

    refactor: update gitignore

--- server/server/settings.py
+++ server/server/settings.py
@@ -138,7 +138,6 @@ DATABASES = {
     #         'ENGINE': 'django.db.backends.sqlite3',
     #         'NAME': BASE_DIR / 'db.sqlite3',
     #     }
-    # }
     # postgres
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
