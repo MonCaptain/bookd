9b8c1cb0a498d0245fe2e115bf2b8695ac256c4e
Shehab Mohsen <97417536+ShehabMohsen@users.noreply.github.com>
2023-06-15 19:53:50 -0400

    Fix filter books (#72)
    * fix: filter out searched books with missing data
    
    * chore: remove import

--- client/src/api/BookAPI.jsx
+++ client/src/api/BookAPI.jsx
@@ -1,5 +1,3 @@
-import unavailable from "../assets/book-cover-unavailable.jpg";
-
 export function bookSearch(bookName) {
   let fetchPromise = Promise.resolve(
     fetch(`https://openlibrary.org/search.json?q=${bookName}`)
@@ -12,13 +10,16 @@ export function searchQuery(bookName) {
     .then((searchResults) => {
       let obtainedBooks = [];
       searchResults.docs.forEach((element) => {
-        if (element.cover_i) {
+        if (
+          element.title &&
+          element.author_name &&
+          element.cover_i &&
+          element.number_of_pages_median
+        ) {
           obtainedBooks.push({
-            title: element.title ? element.title : "Untitled",
-            author: element.author_name ? element.author_name[0] : "Unknown",
-            cover: element.cover_i
-              ? `https://covers.openlibrary.org/b/id/${element.cover_i}-L.jpg`
-              : unavailable,
+            title: element.title,
+            author: element.author_name,
+            cover: `https://covers.openlibrary.org/b/id/${element.cover_i}-L.jpg`,
             pages: element.number_of_pages_median,
             publish_date:
               element.publish_date !== undefined
