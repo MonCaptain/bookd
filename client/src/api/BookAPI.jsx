a06456f88698737210ea538c207b30a10a541327
ShehabMohsen <aquashehab612@gmail.com>
2023-06-16 04:19:01 -0400

    fix: was unable to track books

--- client/src/api/BookAPI.jsx
+++ client/src/api/BookAPI.jsx
@@ -17,9 +17,11 @@ export function searchQuery(bookName) {
           element.number_of_pages_median
         ) {
           obtainedBooks.push({
-            title: element.title,
-            author: element.author_name,
-            cover: `https://covers.openlibrary.org/b/id/${element.cover_i}-L.jpg`,
+            title: element.title ? element.title : "Untitled",
+            author: element.author_name ? element.author_name[0] : "Unknown",
+            cover: element.cover_i
+              ? `https://covers.openlibrary.org/b/id/${element.cover_i}-L.jpg`
+              : unavailable,
             pages: element.number_of_pages_median,
             publish_date:
               element.publish_date !== undefined
